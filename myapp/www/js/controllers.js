d1a7fedb14d48df2ea580d89395cd0ae7dd8f8c1
pranavmayuram <pranav.mayuram@gmail.com>
2015-09-12 07:38:42 -0400

    Add in ionic app as myApp

---
+++ myapp/www/js/controllers.js
@@ -0,0 +1,39 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+	
+    $scope.searchSongs = function(someString) {
+		SC.initialize({
+			client_id: 'f3b6636c2e427ba511f65603ba7448b7'
+		});
+        SC.get('/tracks', {q: someString}, function(tracks) {
+          console.log(tracks);
+          $scope.songs = tracks;
+        });
+	};
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+	// With the new view caching in Ionic, Controllers are only called
+	// when they are recreated or on app start, instead of every page change.
+	// To listen for when this page is active (for example, to refresh data),
+	// listen for the $ionicView.enter event:
+	//
+	//$scope.$on('$ionicView.enter', function(e) {
+	//});
+
+	$scope.chats = Chats.all();
+	$scope.remove = function(chat) {
+		Chats.remove(chat);
+	};
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+	$scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+	$scope.settings = {
+		enableFriends: true
+	};
+});
